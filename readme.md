0b64ce6fefa73193b7801a8a5026e015a1fb1478
LiChris93 <lihaouse@126.com>
2024-03-21 16:06:36 +0800

    first commit

---
+++ readme.md
@@ -0,0 +1,38 @@
+<div align="center">
+
+
+# `translateDemo`
+<p>Python 百度翻译api demo</p>
+<p>
+  <img src="https://forthebadge.com/images/badges/made-with-python.svg">
+  <img src="https://forthebadge.com/images/badges/built-with-love.svg">
+  <br>
+  <img src="https://img.shields.io/badge/Python-3.12.0-blue?style=for-the-badge&logo=appveyor&logo=">
+  <a href="https://www.codefactor.io/repository/github/lichris93/translateDemo"><img src="https://www.codefactor.io/repository/github/lichris93/translateDemo/badge" alt="CodeFactor" /></a>
+  <a href="https://wakatime.com/@09cb58b5-ccc0-41b8-a821-92fbfde0608f"><img src="https://wakatime.com/badge/user/09cb58b5-ccc0-41b8-a821-92fbfde0608f.svg" alt="Total time coded since Jan 13 2023" /></a>
+</p>
+
+
+
+</div>
+
+---
+
+<div align="left">
+
+## 注意事项
+- 请先去[https://api.fanyi.baidu.com/](https://api.fanyi.baidu.com/)申请"通用翻译API"并取得apikey及appid,如图所示,并填入translate.py文件开头
+- <img src="https://img2.imgtp.com/2024/03/21/bTVqrNyx.png"/>
+
+## 关于Python版本
+- [x] 使用 Python3.12 编写，但理论支持所有Python3.x
+
+## TODO
+- [ ] 支持将翻译结果保存到文件
+
+## 声明
+- 该项目使用了百度翻译开放api,我并不是该api的作者,我不对您因使用api而导致的任何问题(包括但不限于账号问题)负责
+- 该项目使用MIT协议(不知道是什么的自行百度)
+- 这玩意纯粹是我自己写着玩的，可能不会频繁更新
+
+</div>
---
+++ translate.py
@@ -0,0 +1,55 @@
+import requests
+import random
+from hashlib import md5
+from urllib import parse
+import json
+
+appID = "appID"  # appID,请设置成自己的
+apiKey = "apikey"  # apikey,请设置成自己的
+
+
+def translate(query, to, source="auto", action=0):
+    """_summary_
+
+    Args:
+        query (String): 要翻译的内容
+        to (String): 目标语言
+        source (str, optional): 来源语言. Defaults to "auto".
+        action (int, optional): 是否用术语库干预翻译结果(需使用高级版及以上的api). Defaults to 0.
+
+    Returns:
+        String: 翻译结果或错误详情
+    """
+    if (
+        not isinstance(query, str)
+        or not isinstance(to, str)
+        or not isinstance(source, str)
+    ):  # 参数错误
+        return "error,illegal argument"
+
+    salt = random.randint(0, 100000)  # 随机salt值
+    sign = md5((appID + query + str(salt) + apiKey).encode()).hexdigest()  # MD5签名
+    data = requests.get(
+        f"https://fanyi-api.baidu.com/api/trans/vip/translate?q={parse.quote(query)}&from={source}&to={to}&appid={appID}&salt={salt}&sign={sign}&action={action}"
+    ).text  # 请求api
+    decoded_data = json.loads(data)  # json->list
+
+    if "error_code" in decoded_data.keys():  # 返回结果里有错误,返回错误详细信息
+        return f"error,code={decoded_data['error_code']},msg=\"{decoded_data['error_msg']}\""
+
+    result = ""
+    for i in decoded_data["trans_result"]:  # 逐行记录翻译结果
+        result += i["dst"]
+        result += "\n"
+    return result  # 返回结果
+
+
+def main():
+    try:
+        print(translate(input("要翻译的内容:"), input("目标语言:")))
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
